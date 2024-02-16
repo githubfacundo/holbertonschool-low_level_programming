59b6679085539691649c0102afc7ece71d0f90a4
githubfacundo <8667@holbertonstudents.com>
2024-02-16 14:51:43 +0000

    C

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 0; a <= 10; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
