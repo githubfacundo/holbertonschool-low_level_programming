8c41e34589073204dc26e14eb0a6c82ae9d8e026
githubfacundo <8667@holbertonstudents.com>
2024-02-16 12:30:58 +0000

    C

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("Last digit of %d is int and is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("Last digit of %d is int and is 0\n", n);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("%dand is less than 6 and not 0\n", n);
+		printf("Last digit of %d is int and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
