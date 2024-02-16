05bdc33e6acc8e45d89a4bdefd694ef23a7c886a
githubfacundo <8667@holbertonstudents.com>
2024-02-16 16:57:54 +0000

    C

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char z;
+for (z = 'z'; a <= 'a'; z--)
+{
+putchar(z);
+}
+putchar('\n');
+return (0);
+}i
