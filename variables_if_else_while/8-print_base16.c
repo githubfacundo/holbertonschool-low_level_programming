abd8d55b69e84e0c6aa67be14c4fd3d41edc15a2
githubfacundo <8667@holbertonstudents.com>
2024-02-20 14:18:30 +0000

    c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
